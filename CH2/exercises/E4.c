6e91237b1512cf599dea5a824486b4b13ff1c80c
Gaspar Fabrega Ragni <gfabrega@eso.org>
2025-10-01 23:46:07 -0300

    reorganizing files

---
+++ CH2/exercises/E4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int i1, i2, i3, i4;
+    float f1, f2, f3, f4;
+
+    printf("integers: %d, %d, %d, %d\n", i1, i2, i3, i4);
+    printf("floats: %.2f, %.2f, %.2f, %.2f\n", f1, f2, f3, f4);
+
+    return 0;
+}
