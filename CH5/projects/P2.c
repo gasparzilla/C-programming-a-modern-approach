5ec83673cf70af586ffaf85fbbac8df8a9b08e34
gasparzilla <gasparfab@gmail.com>
2026-02-18 14:19:25 -0300

    fix wrong parsing at 12:00 in project 2

--- CH5/projects/P2.c
+++ CH5/projects/P2.c
@@ -6,7 +6,10 @@ int main(void){
     printf("Enter a 24-hour time (HH:MM): ");
     scanf("%2d: %2d", &hour, &minute);
     
-    if (hour > 12){
+    if (hour == 12){
+        printf("Equivalent 12-hour time: %.2d:%.2d PM\n", hour, minute);
+    }
+    else if(hour > 12){
         hour = hour - 12;
         printf("Equivalent 12-hour time: %.2d:%.2d PM\n", hour, minute);
     }
