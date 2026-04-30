599499d3bee909e110cc6e15181542e4ce13126c
gasparzilla <gasparfab@gmail.com>
2026-04-30 16:25:49 -0400

    problem 10 done

--- CH5/projects/P10.c
+++ CH5/projects/P10.c
@@ -4,20 +4,29 @@ int main(void){
     
     char letter = 'F';
 
-    
-
-    switch(grade_head){
-        case 10: case 9:
-            letter = 'A';
-            break;
-        case 8:
-            letter = 'B';
-            break;
-        case 7
-            letter = 'C';
-            break
-        case 6:
-            letter = 'D';
-            break
+    int grade, grade_head;
+    printf("Enter a numerical grade (0-100): ");
+    scanf("%d", &grade);
+    if (grade > 100 || grade < 0){
+        printf("grades can't be greater than 100 or less than 0!\n");
     }
+    else {
+        grade_head = grade / 10;
+        switch(grade_head){
+            case 10: case 9:
+                letter = 'A';
+                break;
+            case 8:
+                letter = 'B';
+                break;
+            case 7:
+                letter = 'C';
+                break;
+            case 6:
+                letter = 'D';
+                break;
+        }
+        printf("Letter grade: %c\n", letter);
+    }
+    return 0;
 }
