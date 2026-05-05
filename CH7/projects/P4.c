b4ae321e375d3c3c3cae86b2f242a0f986ea96f0
Gaspar Fabrega Ragni <gfabrega@eso.org>
2026-05-05 15:35:42 -0400

    starting project 4 of chapter 7

--- CH7/projects/P4.c
+++ CH7/projects/P4.c
@@ -2,11 +2,42 @@
 #include <ctype.h>
 
 int main(void){
-    
-    toupper(ch);
+    char ch;
 
-    switch (ch){
-        case 'A': case 'B': case 'C'
+	printf("Enter a character: ");
+	ch = getchar();
+    ch = toupper(ch);
 
+	while(ch != '\n') {
+    	switch (ch){
+        	case 'A': case 'B': case 'C':
+        		putchar('2');
+        		break;
+        	case 'D': case 'E': case 'F':
+        		putchar('3');
+        		break;
+        	case 'G': case 'H': case 'I':
+        		putchar('4');
+        		break;
+        	case 'J': case 'K': case 'L':
+        		putchar('5');
+        		break;
+        	case 'M': case 'N': case 'O':
+        		putchar('6');
+        		break;
+        	case 'P': case 'Q': case 'R': case 'S':
+        		putchar('7');
+        		break;
+       		case 'T': case 'U': case 'V':
+       		 	putchar('8');
+       		 	break;
+       		case 'W': case 'X': case 'Y':
+          		putchar('9');
+          		break;
+          	}
+        ch = getchar();
+        ch = toupper(ch);
+	}
+	
     return 0;
 }
