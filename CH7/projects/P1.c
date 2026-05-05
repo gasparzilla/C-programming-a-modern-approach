4c71533bdd613b2944e8f127b1b3294a357d8344
gasparzilla <gasparfab@gmail.com>
2026-05-05 09:46:37 -0400

    project 1 CH7 done

--- CH7/projects/P1.c
+++ CH7/projects/P1.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
 int main(void){
-    int i, n;
+    // int i, n; // max n before overflow is 46340
+    //short i, n; // max n before overflow is 181
+    long i, n; // max n before overflow is 3037000499
 
-    printf("This program prints a table");
-    
+    printf("This program prints a table of squares.\n");
+    printf("Enter number of entries in table: ");
+    // scanf("%d", &n);
+    //scanf("%hd", &n);
+    scanf("%ld", &n);
+
+    for (i = 1; i<= n; i++){
+        // printf("%15d%15d\n", i, i*i);
+        // printf("%8hd%8hd\n", i, i*i);
+    }
+
+    // Takes too long to finish for long
+    printf("%20ld%20ld\n", n, n*n);
+
     return 0;
 }
