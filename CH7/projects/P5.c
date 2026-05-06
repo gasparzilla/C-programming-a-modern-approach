64a4ecf8e7cc5a69930af0dadc3190b79ed4cf27
gasparzilla <gasparfab@gmail.com>
2026-05-05 20:31:41 -0400

    project 5 CH7 done

---
+++ CH7/projects/P5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void){
+    char ch;
+    int points = 0;
+
+	printf("Enter a word: ");
+	ch = getchar();
+    ch = toupper(ch);
+
+	while(ch != '\n') {
+    	switch (ch){
+            case 'A': case 'E': case 'I': case 'L': case 'N':
+            case 'O': case 'R': case 'S': case 'T': case 'U':
+        		points += 1;
+        		break;
+        	case 'D': case 'G':
+        		points += 2;
+        		break;
+            case 'B': case 'C': case 'M': case 'P':
+        		points += 3;
+        		break;
+            case 'F': case 'H': case 'V': case 'W': case 'Y':
+        		points += 4;
+        		break;
+        	case 'K':
+        		points += 5;
+        		break;
+        	case 'J': case 'X':
+                points += 8;
+        		break;
+        	case 'Q': case 'Z':
+                points += 10;
+        		break;
+          	}
+        ch = getchar();
+        ch = toupper(ch);
+	}
+	printf("Scrabble value: %d\n", points);
+
+    return 0;
+}
