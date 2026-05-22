19046bcf2d9e31f12b47dab1c9860dd5def43ad8
gasparzilla <gasparfab@gmail.com>
2026-05-22 12:41:53 -0400

    project 10 chapter 8 done

--- CH8/projects/P10.c
+++ CH8/projects/P10.c
@@ -2,44 +2,41 @@
 
 int main(void){
     int hours, minutes, mtm;
+    int d_schedule[8] = {8*60,
+                         9*60+43,
+                         11*60+19,
+                         12*60+47,
+                         14*60,
+                         15*60+45,
+                         19*60,
+                         21*60+45};
+    int a_schedule[8] = {10*60+16,
+                         11*60+52,
+                         13*60+31,
+                         15*60,
+                         16*60+8,
+                         17*60+55,
+                         21*60+20,
+                         23*60+58};
 
     printf("Enter a 24-hour time: ");
     scanf("%2d:%2d", &hours, &minutes);
 
     mtm = 60*hours + minutes;
 
-    int d_hours, d_minutes = 0, a_hours, a_minutes = 0;
-    if (mtm < (((8*60) + (9*60+43)) / 2)){
-        d_hours = 8;
-        a_hours = 10; a_minutes = 16;
-    }
-    else if (mtm < (((9*60+43) + (11*60+19)) / 2)){
-        d_hours = 9; d_minutes = 43;
-        a_hours = 11; a_minutes = 52;
-    }   
-    else if (mtm < (((11*60+19) + (12*60+47)) / 2)){
-        d_hours = 11; d_minutes = 19;
-        a_hours = 13; a_minutes = 31;
-    }
-    else if (mtm < (((12*60+47) + (14*60)) / 2)){
-        d_hours = 12; d_minutes = 47;
-        a_hours = 3;
-    }
-    else if (mtm < (((14*60) + (15*60+45)) / 2)){
-        d_hours = 14;
-        a_hours = 16; a_minutes = 8;
-    }
-    else if (mtm < (((15*60+45) + (19*60)) / 2)){
-        d_hours = 15; d_minutes = 45;
-        a_hours = 17; a_minutes = 55;
-    }
-    else if (mtm < (((19*60) + (21*60+45)) / 2)){
-        d_hours = 19;
-        a_hours = 21; a_minutes = 20;
-    }
-    else {
-        d_hours = 21; d_minutes = 45;
-        a_hours = 23; a_minutes = 58;
+    int d_hours = d_schedule[7] / 60,
+        d_minutes = d_schedule[7] % 60,
+        a_hours = a_schedule[7] / 60,
+        a_minutes = a_schedule[7] % 60;
+
+    for (int i = 0; i < 7; i++){
+        if (mtm < ((d_schedule[i] + d_schedule[i+1]) / 2)){
+            d_hours = d_schedule[i] / 60;
+            d_minutes = d_schedule[i] % 60;
+            a_hours = a_schedule[i] / 60;
+            a_minutes = a_schedule[i] % 60;
+            break;
+        }
     }
 
     if ((d_hours < 12) && (a_hours < 12)){
