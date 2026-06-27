a358b8a0c1d1af12dff9d98ed7de1b7b3e49f888
gasparzilla <gasparfab@gmail.com>
2026-06-26 20:35:03 -0400

    updating couple of exercises from chapter 9

--- CH9/exercises.c
+++ CH9/exercises.c
@@ -33,7 +33,6 @@ int gcd(int i1, int i2){
 }
 
 // E4
-
 int day_of_year(int month, int day, int year){
     int n_day = day, feb_days = 28;
     if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ){
@@ -49,10 +48,39 @@ int day_of_year(int month, int day, int year){
     return n_day;
 }
 
+// E5
+int num_digits(int n){
+    int digits = 0;
+    while (n > 0){
+        digits++;
+        n = n % 10;
+    }
+    return digits;
+}
+
+// E6
+int digit(int n, int k){
+    int d = 0;
+    while (k > 1){
+        k--;
+        n = n / 10;
+    }
+    return n % 10;
+}
+
+// E7
+int largest_element(int[])
+
 int main(void){
-    printf("%d\n", day_of_year(1, 1, 1996));
-    printf("%d\n", day_of_year(12, 31, 1996));
-    printf("%d\n", day_of_year(12, 31, 2000));
-    printf("%d\n", day_of_year(12, 31, 2001));
+    printf("date 1: %d\n", day_of_year(1, 1, 1996));
+    printf("date 2: %d\n", day_of_year(12, 31, 1996));
+    printf("date 3: %d\n", day_of_year(12, 31, 2000));
+    printf("date 4: %d\n", day_of_year(12, 31, 2001));
+
+    printf("digit 1: %d\n", digit(123456789, 1));
+    printf("digit 1: %d\n", digit(123456789, 3));
+    printf("digit 1: %d\n", digit(123456789, 5));
+    printf("digit 1: %d\n", digit(123456789, 9));
+    printf("digit 1: %d\n", digit(123456789, 12));
     return 0;
 }
